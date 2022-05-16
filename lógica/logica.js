fa43359e6cd379b709bb5ecaa44d81cc83d7e921
Peeterlopez <pedrolopezcabral@gmail.com>
2022-05-16 20:39:08 -0300

    todo junto

---
+++ lógica/logica.js
@@ -0,0 +1,21 @@
+function precio() {
+    
+let tipo = document.getElementById("tipoticket").value;
+let cantidad = document.getElementById("cantidad").value;
+let valor = cantidad * 200;
+
+if(tipo == "Estudiante"){
+    valor= valor * 0.2;
+}
+else if( tipo == "Trainee"){
+    valor= valor * 0.5;
+}
+else{
+    valor = valor * 0.85;
+}
+
+document.getElementById("precio").innerHTML=" Total a Pagar: $" + valor;
+}
+
+let resumen= document.getElementById("resumen");
+resumen.onclick = precio;
